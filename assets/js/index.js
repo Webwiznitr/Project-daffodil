b2ed40dbb5e337485e1ae0be499e4ffda0c9eac3
Sayna Parida <saynaparida33@gmail.com>
2020-10-07 20:26:00 +0530

    Basic structure added

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
             $(".menu-icon").on("click", function() {
                   $("nav ul").toggleClass("showing");
             });
+    
+            $(".electrical-btn").click(function () {
+                $(".electrical").show();
+                $(".electronics").hide();
+            });
+            $(".electronics-btn").click(function () {
+                $(".electronics").show();
+                $(".electrical").hide();
+            });
       });
 
       // Scrolling Effect
