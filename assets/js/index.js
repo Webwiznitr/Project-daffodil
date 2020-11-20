5ed4c66f53ad5298700c7547c256d8cf06ead4d0
Sayna Parida <saynaparida33@gmail.com>
2020-11-20 20:26:37 +0530

    Added contact us section

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,4 +27,11 @@ $(document).ready(function() {
             }
       })
 
+      $( '.js-input' ).keyup(function() {
+            if( $(this).val() ) {
+               $(this).addClass('not-empty');
+            } else {
+               $(this).removeClass('not-empty');
+            }
+          });
 
